0dd3f7e2d6ed2c7ec0e13a8ec42e298ff6d811b6
Veronica Stork <zealous.anemone@gmail.com>
2021-04-28 19:53:58 -0400

    Add ability to pull from FCC blog

--- pages/writing.js
+++ pages/writing.js
@@ -1,10 +1,23 @@
 import Header from '../components/Header';
+let Parser = require('rss-parser');
+let parser = new Parser();
+const getFeed = async () => {
+  const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+  let feed = await parser.parseURL(
+    CORS_PROXY + 'https://www.freecodecamp.org/news/author/veronica/rss/'
+  );
+  console.log(feed.title);
 
+  feed.items.forEach((item) => {
+    console.log(item.title + ':' + item.link);
+  });
+};
 const Writing = () => {
   return (
     <div>
       <Header />
       <h1>Stuff I've Written</h1>
+      <button onClick={getFeed}>get feed</button>
     </div>
   );
 };
