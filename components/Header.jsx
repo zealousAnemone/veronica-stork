a0d6fca6e93ddbfe89ab14860a51eca1887302e6
Veronica Stork <zealous.anemone@gmail.com>
2021-07-08 17:12:45 -0400

    Makes menu responsive

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,12 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Header = () => {
+  const [dropdown, setDropdown] = useState(false);
+  const toggleDropdown = () => {
+    setDropdown(!dropdown);
+  }
+
   return (
     <header>
       <nav>
@@ -9,6 +15,15 @@ const Header = () => {
         <Link href="/writing"><a>Writing</a></Link>
         <Link href="/contact"><a>Contact Me</a></Link>
       </nav>
+      <span className="hamberder">
+        <img src="/hamberder.png" onClick={toggleDropdown} />
+      </span>
+      {dropdown && <div id="dropdown">
+ <Link href="/" ><a onClick={toggleDropdown}>Home</a></Link>
+ <Link href="/about" ><a onClick={toggleDropdown}>About Me</a></Link>
+ <Link href="/writing" ><a onClick={toggleDropdown}>Writing</a></Link>
+ <Link href="/contact" ><a onClick={toggleDropdown}>Contact Me</a></Link>
+</div>} 
     </header>
   )
 }
